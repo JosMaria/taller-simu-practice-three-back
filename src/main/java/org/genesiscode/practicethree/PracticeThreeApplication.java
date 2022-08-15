44405e2934139fbffd93591b514200960760887b
Jose Maria <josmariaguilar@gmail.com>
2022-08-15 19:28:49 -0400

    Initial commit

---
+++ src/main/java/org/genesiscode/practicethree/PracticeThreeApplication.java
@@ -0,0 +1,13 @@
+package org.genesiscode.practicethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/genesiscode/practicethree/PracticeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.genesiscode.practicethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
